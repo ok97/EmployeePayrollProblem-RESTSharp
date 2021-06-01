7c63c52fd3fd9ec17332ed27e9c429f8f64e159e
ok97 <omkhawshi0@gmail.com>
2021-06-02 00:28:53 +0530

    Added: Retrieve all Employee Details Json DB

---
+++ EmployeePayrollProblem-RESTSharp/EmployeeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollProblem_RESTSharp
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+    }
+}
---
+++ EmployeePayrollProblem-RESTSharp/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace EmployeePayrollProblem_RESTSharp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        RestClient client;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            //Initialize the base URL to execute requests made by the instance
+            client = new RestClient("http://localhost:3000");
+        }
+        private IRestResponse GetEmployeeList()
+        {
+            //Arrange
+            //Initialize the request object with proper method and URL
+            RestRequest request = new RestRequest("/Employees", Method.GET);
+            //Act
+            // Execute the request
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+
+        /* UC1 Retrieve all employee details in the json DB*/
+
+        [TestMethod]
+        public void OnCallingGetAPI_ReturnEmployeeList()
+        {
+            IRestResponse response = GetEmployeeList();
+            // Check if the status code of response equals the default code for the method requested
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            // Convert the response object to list of employees
+            List<EmployeeModel> employeeList = JsonConvert.DeserializeObject<List<EmployeeModel>>(response.Content);
+            Assert.AreEqual(5, employeeList.Count);
+            foreach (EmployeeModel emp in employeeList)
+            {
+                Console.WriteLine("Id:- " + emp.Id + "\t" + "Name:- " + emp.Name + "\t" + "Salary:- " + emp.Salary);
+            }
+        }
+    }
+}
+
