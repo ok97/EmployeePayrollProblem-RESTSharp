117ec84a89be627f134a2ec75dae920b0718b9e7
ok97 <omkhawshi0@gmail.com>
2021-06-02 00:54:22 +0530

    Added: Add multiple Employee Json Db

---
+++ EmployeePayrollProblem-RESTSharp/Employees.cs
@@ -0,0 +1,16 @@
+﻿namespace EmployeePayrollProblem_RESTSharp
+{
+    internal class Employees : EmployeeModel
+    {
+        private int v1;
+        private string v2;
+        private string v3;
+
+        public Employees(int v1, string v2, string v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+    }
+}
