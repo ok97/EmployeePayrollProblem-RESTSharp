47bc9cbf2f3dbc68d3a1f4a27260c52ce25c6917
ok97 <omkhawshi0@gmail.com>
2021-06-07 15:42:32 +0530

    Added: Add a New Employee json DB

--- EmployeePayrollProblem-RESTSharp/UnitTest1.cs
+++ EmployeePayrollProblem-RESTSharp/UnitTest1.cs
@@ -52,6 +52,37 @@ namespace EmployeePayrollProblem_RESTSharp
 
 
 
+        /* UC2:- Ability to add a new Employee to the EmployeePayroll JSON Server.
+                 - Use JSON Server and RESTSharp to save the EmployeePayroll Data of id, name, and salary.
+                 - Ability to add using RESTSharp to JSONServer in the MSTest Test Case and then on success add to Employee Payroll .
+                 - Validate with the successful Count 
+        */
+
+        [TestMethod]
+        public void OnCallingPostAPI_ReturnEmployeeObject()
+        {
+            // Arrange
+            // Initialize the request for POST to add new employee
+            RestRequest request = new RestRequest("/Employees", Method.POST);
+            JsonObject jsonObj = new JsonObject();
+            jsonObj.Add("id", 3);
+            jsonObj.Add("name", "Jimmy");
+            jsonObj.Add("salary", "150000");
+            
+            // Added parameters to the request object such as the content-type and attaching the jsonObj with the request
+            request.AddParameter("application/json", jsonObj, ParameterType.RequestBody);
+
+            //Act
+            IRestResponse response = client.Execute(request);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+            EmployeeModel employee = JsonConvert.DeserializeObject<EmployeeModel>(response.Content);
+            Assert.AreEqual("Jimmy", employee.Name);
+            Assert.AreEqual("150000", employee.Salary);
+            Console.WriteLine(response.Content);
+        }
+
         
     }
 }
